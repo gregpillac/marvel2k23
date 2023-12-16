c74d5d0586480912cfd83b586c94d358286f93c0
gregpiac <gregory.pillac@gmail.com>
2023-12-16 18:13:32 +0100

    definition des attributs de carte

--- marvels2k23/src/app/card/card.component.ts
+++ marvels2k23/src/app/card/card.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
+  id!: number;
+  title!: string;
+  description!: string;
+  imagesource!: string;
+  capacitetype!: string;
+  capacitevaleur!: number;
+
+  ngOnInit(): void {
+    this.id = 74;
+    this.title = 'RHINO';
+    this.description = 'rhino';
+    this.imagesource = 'assets/images/mv064.jpg';
+    this.capacitetype = 'COURAGE PUISSANCE INTELLIGENCE';
+    this.capacitevaleur = 6;
+  }
 }
